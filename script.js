117af04d67f1fc29b8aa72af33248419fd648621
denis-ttk-1975 <denis.ttk@gmail.com>
2021-06-14 11:45:40 +0300

    fulfilled in solo mode

--- script.js
+++ script.js
@@ -15,3 +15,117 @@
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+alert('Practice Task #4');
+
+let numberOfFilms = 0;
+
+function start() { 
+
+do {numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?????!!!!!!!!!!');}
+while (numberOfFilms<=0 || numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms));
+
+alert(numberOfFilms);
+}
+
+start();
+
+let personalMovieDB = {count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+	rememberMyFilms: function () {
+		let filmName = '';
+		let filmScore = '';
+		
+		for (let i=0; i<=1; i++)  {
+			do {filmName = prompt('Один из последних просмотренных фильмов?');} 
+			while ( filmName == '' || filmName == 0 || filmName == null || filmName.length > 50);
+			do {filmScore = prompt('На сколько оцените его?');}
+			while ( filmScore == '' || filmScore == 0 || filmScore == null || +filmScore > 100);
+			
+			personalMovieDB.movies[filmName] = filmScore;
+		}
+	},
+	detectPersonalLevel: function() {
+		if (personalMovieDB.count < 10) {
+			alert ('Просмотрено довольно мало фильмов');
+		} else if ((personalMovieDB.count >= 10) || (personalMovieDB.count < 30)) {
+			alert ('Вы классический зритель');
+		} else if (personalMovieDB.count >= 30) {
+			alert ('Вы киноман');
+		} else {alert ('Произошла ошибка');}
+	},
+	showMyDB: function() {
+		if (!personalMovieDB.privat) {
+			console.log(personalMovieDB.privat, personalMovieDB);
+		}
+	},
+	writeYourGenres: function() {
+		for (let i = 1; i <= 3; i++) {
+			let a = prompt(`Ваш любимый жанр под номером ${i}`);
+			if (a == null || a == '') {
+				i--;
+				continue;
+			}
+			personalMovieDB.genres[i-1] = a;
+		}
+	},
+	toggleVisibleMyDB: function() {
+		if (personalMovieDB.privat == false) {
+			personalMovieDB.privat = true; 
+		}
+		else {
+			personalMovieDB.privat = false;
+		}
+	}
+
+};
+personalMovieDB.rememberMyFilms();
+personalMovieDB.writeYourGenres();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB();
+
+personalMovieDB.toggleVisibleMyDB();
+
+
+
+// function rememberMyFilms() {
+
+// let filmName = '';
+// let filmScore = '';
+
+// for (let i=0; i<=1; i++)  {
+
+// 	do {filmName = prompt('Один из последних просмотренных фильмов?');} 
+// 	while ( filmName == '' || filmName == 0 || filmName == null || filmName.length > 50);
+// 	do {filmScore = prompt('На сколько оцените его?');}
+// 	while ( filmScore == '' || filmScore == 0 || filmScore == null || +filmScore > 100);
+	
+// 	personalMovieDB.movies[filmName] = filmScore;
+// }
+
+// console.log(personalMovieDB);
+
+// let i1 = 0;
+
+// while (i1<2)  {
+
+// 	do {filmName = prompt('Один из последних просмотренных фильмов?');} 
+// 	while ( filmName == '' || filmName == 0 || filmName == null || filmName.length > 50);
+// 	do {filmScore = prompt('На сколько оцените его?');}
+// 	while ( filmScore == '' || filmScore == 0 || filmScore == null || +filmScore > 100);
+	
+// 	personalMovieDB.movies[filmName] = filmScore;
+// 	i1++;
+// }
+
+// console.log(personalMovieDB);
+
+// }
+
+// rememberMyFilms();
+
+console.log(personalMovieDB);
+personalMovieDB.showMyDB();
